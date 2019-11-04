ff4c855d3719c6b78ff1d1ef3bdb5e1b26b4a014
Leon <FeelineKing@gmail.com>
2019-11-04 11:49:16 +0200

    Updates

---
+++ precache-manifest.6f80c18e59ddea01aa37386d4a0e0597.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a79bdf36b427499888fcbd79adabcae",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "478e58fe5287ebef02ab",
+    "url": "/react_list-of-goods/static/css/main.e033d634.chunk.css"
+  },
+  {
+    "revision": "aa19a42a60f1b42b0a5d",
+    "url": "/react_list-of-goods/static/js/2.26a51a8b.chunk.js"
+  },
+  {
+    "revision": "478e58fe5287ebef02ab",
+    "url": "/react_list-of-goods/static/js/main.0d55fb89.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
